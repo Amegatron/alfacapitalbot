4483e2d4421fd75674541e58ad6be4228daf52cb
amega <amego2006@gmail.com>
2018-03-10 05:22:46 +0300

    SetMyCallbackCommand implementation

--- app/Telegram/CallbackCommands/SetMyCallbackCommand.php
+++ app/Telegram/CallbackCommands/SetMyCallbackCommand.php
@@ -1,23 +1,61 @@
 <?php
 namespace App\Telegram\CallbackCommands;
 
+use App\Opif;
+use Telegram\Bot\Keyboard\Keyboard;
+
 class SetMyCallbackCommand extends CallbackCommand
 {
     protected $name = 'setmy';
 
+    protected $inputPif;
+
     public function getParameters()
     {
-        return [];
+        return [$this->inputPif];
     }
 
     public function setParameters($params)
     {
-        return;
+        $this->inputPif = $params[0];
+    }
+
+    public function setInputPif($pif = 0)
+    {
+        $this->inputPif = $pif;
     }
 
     public function handle()
     {
         $this->answerCallbackQuery();
-        $this->editMessageText(['text' => 'not implemented yet']);
+
+        if (!$this->inputPif) {
+            /** @var Opif $pifs */
+            $pifs = Opif::orderBy('id', 'asc')->get();
+
+            $keyboard = Keyboard::make()->inline();
+            foreach ($pifs as $pif) {
+                /** @var SetMyCallbackCommand $command */
+                $command = app(SetMyCallbackCommand::class);
+                $command->setInputPif($pif->id);
+
+                $button = Keyboard::inlineButton([
+                    'text' => $pif->fullName,
+                    'callback_data' => $command->getCallbackData(),
+                ]);
+                $keyboard->row($button);
+            }
+
+            $this->editMessageText([
+                'text' => 'Выберите ПИФ:',
+                'reply_markup' => $keyboard,
+            ]);
+        } else {
+            /** @var Opif $opif */
+            $opif = Opif::find($this->inputPif);
+            $this->replyWithMessage([
+                'text' => 'Пришлите мне сообщением Ваше кол-во паев в выбранном ПИФе ("' . $opif->name . '""):',
+            ]);
+        }
     }
 }
