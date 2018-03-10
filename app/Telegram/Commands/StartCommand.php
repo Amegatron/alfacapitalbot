ebedb0530303cd695eea15d0923cd423f231d625
amega <amego2006@gmail.com>
2018-03-10 12:22:29 +0300

    Test

--- app/Telegram/Commands/StartCommand.php
+++ app/Telegram/Commands/StartCommand.php
@@ -14,15 +14,13 @@ class StartCommand extends Command
     public function handle($arguments)
     {
         $keyboard = Keyboard::make();
-
         $button1 = Keyboard::button([
             'text' => 'Text1',
         ]);
         $button2 = Keyboard::button([
             'text' => 'Text2',
         ]);
-
-        $keyboard->row([$button1, $button2]);
+        $keyboard->row($button1, $button2);
 
         $this->replyWithMessage([
             'text' => 'Hello!',
