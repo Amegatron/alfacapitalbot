a91e3f141d9c4461bdb0d51d4f2fdf73fb04e1ad
amega <amego2006@gmail.com>
2018-04-07 02:47:27 +0300

    Removed Auth controllers New test for CallbackCommandBus

--- tests/Unit/CallbackCommandBusTest.php
+++ tests/Unit/CallbackCommandBusTest.php
@@ -36,4 +36,28 @@ class CallbackCommandBusTest extends TestCase
         $bus->addCommand($commandMock);
         $bus->handle("testcommand:test", $updateMock);
     }
+
+    /** @test */
+    public function a_bus_should_not_call_the_command()
+    {
+        $telegramMock = \Mockery::mock(Api::class);
+        $updateMock = \Mockery::mock(Update::class);
+
+        $commandMock = \Mockery::mock(CallbackCommand::class, [$telegramMock])->makePartial();
+        $commandName = "testcommand";
+        $commandMock->shouldReceive("getName")->andReturn($commandName);
+        $commandMock->shouldReceive("setParameters")
+            ->never()
+            ->andReturnSelf();
+        $commandMock->shouldReceive("handle")->never();
+
+        $this->app->bind(get_class($commandMock), function ($app) use ($commandMock) {
+            return $commandMock;
+        });
+
+        $bus = new CallbackCommandBus($telegramMock);
+        $bus->addCommand($commandMock);
+        $bus->removeCommands([$commandName]);
+        $bus->handle("testcommand:test", $updateMock);
+    }
 }
