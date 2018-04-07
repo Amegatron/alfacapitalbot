329a6ca1e056ac94835e4b3cf20f7b0a68af5f4e
amega <amego2006@gmail.com>
2018-04-07 21:56:33 +0300

    CallbackCommand additional tests

--- tests/Unit/CallbackCommands/BaseCommandTest.php
+++ tests/Unit/CallbackCommands/BaseCommandTest.php
@@ -3,6 +3,10 @@ namespace Tests\Unit\CallbackCommands;
 
 use App\Telegram\CallbackCommands\CallbackCommand;
 use Telegram\Bot\Api;
+use Telegram\Bot\Objects\CallbackQuery;
+use Telegram\Bot\Objects\Chat;
+use Telegram\Bot\Objects\Message;
+use Telegram\Bot\Objects\Update;
 use Tests\TestCase;
 
 class BaseCommandTest extends TestCase
@@ -44,5 +48,134 @@ class BaseCommandTest extends TestCase
         $this->expectException(\InvalidArgumentException::class);
 
         $command->getCallbackData();
+    }
+
+    /** @test */
+    public function a_command_should_not_call_answer_callback()
+    {
+        $telegram = \Mockery::mock(Api::class)->makePartial();
+
+        $update = \Mockery::mock(Update::class)->makePartial();
+        $update->shouldReceive("getCallbackQuery")->andReturnNull();
+
+        $command = \Mockery::mock(CallbackCommand::class, [$telegram])->makePartial();
+
+        $this->expectException(\BadMethodCallException::class);
+        $this->expectExceptionMessage("CallbackQuery is missing in request");
+
+        $command->setUpdate($update);
+        $command->answerCallbackQuery([]);
+    }
+
+    /** @test */
+    public function a_command_should_call_answer_callback()
+    {
+        $queryId = 1;
+
+        $telegram = \Mockery::mock(Api::class)->makePartial();
+        $telegram->shouldReceive("answerCallbackQuery")
+            ->once()
+            ->withArgs(function($params) use ($queryId) {
+                $this->assertTrue(is_array($params));
+                $this->assertArrayHasKey("callback_query_id", $params);
+                $this->assertEquals($queryId, $params['callback_query_id']);
+                return true;
+            });
+
+        $callbackQuery = \Mockery::mock(CallbackQuery::class)->makePartial();
+        $callbackQuery->shouldReceive("getId")->andReturn($queryId);
+
+        $update = \Mockery::mock(Update::class)->makePartial();
+        $update->shouldReceive("getCallbackQuery")->andReturn($callbackQuery);
+
+        $command = \Mockery::mock(CallbackCommand::class, [$telegram])->makePartial();
+
+        $command->setUpdate($update);
+        $command->answerCallbackQuery([]);
+    }
+
+    /** @test */
+    public function a_command_should_call_edit_message_text_1()
+    {
+        $inlineMessageId = 1;
+
+        $telegram = \Mockery::mock(Api::class)->makePartial();
+        $telegram->shouldReceive("editMessageText")
+            ->once()
+            ->withArgs(function($params) use ($inlineMessageId) {
+                $this->assertTrue(is_array($params));
+                $this->assertArrayHasKey("inline_message_id", $params);
+                $this->assertEquals($inlineMessageId, $params["inline_message_id"]);
+                return true;
+            });
+
+        $message = \Mockery::mock(Message::class)->makePartial();
+
+        $callbackQuery = \Mockery::mock(CallbackQuery::class)->makePartial();
+        $callbackQuery->shouldReceive("getMessage")->andReturn($message);
+        $callbackQuery->shouldReceive("getInlineMessageId")->andReturn($inlineMessageId);
+
+        $update = \Mockery::mock(Update::class)->makePartial();
+        $update->shouldReceive("getCallbackQuery")->andReturn($callbackQuery);
+
+        $command = \Mockery::mock(CallbackCommand::class, [$telegram])->makePartial();
+        $command->setUpdate($update);
+        $command->editMessageText([]);
+    }
+
+    /** @test */
+    public function a_command_should_call_edit_message_text_2()
+    {
+        $chatId = 1;
+        $messageId = 10;
+
+        $telegram = \Mockery::mock(Api::class)->makePartial();
+        $telegram->shouldReceive("editMessageText")
+            ->once()
+            ->withArgs(function($params) use ($chatId, $messageId) {
+                $this->assertTrue(is_array($params));
+                $this->assertArrayHasKey("chat_id", $params);
+                $this->assertArrayHasKey("message_id", $params);
+                $this->assertEquals($chatId, $params["chat_id"]);
+                $this->assertEquals($messageId, $params["message_id"]);
+                return true;
+            });
+
+        $chat = \Mockery::mock(Chat::class)->makePartial();
+        $chat->shouldReceive("getId")->andReturn($chatId);
+
+        $message = \Mockery::mock(Message::class)->makePartial();
+        $message->shouldReceive("getChat")->andReturn($chat);
+        $message->shouldReceive("getMessageId")->andReturn($messageId);
+
+        $callbackQuery = \Mockery::mock(CallbackQuery::class)->makePartial();
+        $callbackQuery->shouldReceive("getMessage")->andReturn($message);
+        $callbackQuery->shouldReceive("getInlineMessageId")->andReturn(0);
+
+        $update = \Mockery::mock(Update::class)->makePartial();
+        $update->shouldReceive("getCallbackQuery")->andReturn($callbackQuery);
+
+        $command = \Mockery::mock(CallbackCommand::class, [$telegram])->makePartial();
+        $command->setUpdate($update);
+        $command->editMessageText([]);
+    }
+
+    /** @test */
+    public function a_command_should_throw_exception()
+    {
+        $telegram = \Mockery::mock(Api::class)->makePartial();
+
+        $callbackQuery = \Mockery::mock(CallbackQuery::class)->makePartial();
+        $callbackQuery->shouldReceive("getMessage")->andReturnNull();
+
+        $update = \Mockery::mock(Update::class)->makePartial();
+        $update->shouldReceive("getCallbackQuery")->andReturn($callbackQuery);
+
+        $this->expectException(\BadMethodCallException::class);
+        $this->expectExceptionMessage("No callbackQuery available for editMessageText");
+
+        $command = \Mockery::mock(CallbackCommand::class, [$telegram])->makePartial();
+        $command->setUpdate($update);
+        $command->editMessageText([]);
     }
 }
